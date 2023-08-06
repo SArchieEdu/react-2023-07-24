dc2aee8cfb703e9bae6026da8f0cabbf7ded173e
Igor Dostovalov <igor.dostovalov@devim.team>
2023-08-06 17:07:26 +0500

    Tabs onClick

--- src/components/Tabs/component.jsx
+++ src/components/Tabs/component.jsx
@@ -1,20 +1,12 @@
 import { Button } from '../../components/Button/component';
 
-export const Tabs = ({ restaurants, onTabSelect }) => {
-  const handleClick = (index) => {
-    if (onTabSelect) {
-      onTabSelect(index);
-    }
-  };
-
-  return (
-    <div>
-      {restaurants &&
-        restaurants.map((restaurant, index) => (
-          <Button key={restaurant.id} onClick={() => handleClick(index)}>
-            {restaurant.name}
-          </Button>
-        ))}
-    </div>
-  );
-};
+export const Tabs = ({ restaurants, onTabSelect }) => (
+  <div>
+    {restaurants &&
+      restaurants.map((restaurant, index) => (
+        <Button key={restaurant.id} onClick={() => onTabSelect(index)}>
+          {restaurant.name}
+        </Button>
+      ))}
+  </div>
+);
