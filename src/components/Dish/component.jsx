cceb44ebed5fbd1c7f78093cfaab31082e62f435
Igor Dostovalov <igor.dostovalov@devim.team>
2023-08-06 18:47:21 +0500

    HT-2

--- src/components/Dish/component.jsx
+++ src/components/Dish/component.jsx
@@ -1,3 +1,26 @@
+import { useState } from 'react';
+
+
 export const Dish = ({ dish }) => {
-  return <div>{dish.name}</div>;
+  const [count, setCount] = useState(0);
+
+  const toggleDish = (type) => {
+    if(type === 'add') {
+      setCount(count => count + 1);
+    } 
+
+    if(type === 'remove' && count > 0) {
+      setCount(count => count - 1);
+    }
+  }
+
+  return <div>
+    {dish.name}
+    <div>
+      <button onClick={() => toggleDish('remove')}>-</button>
+      <span>{count}</span>
+      <button onClick={() => toggleDish('add')}>+</button>
+    </div>
+    price: {Number(dish.price) *  count}
+    </div>;
 };
