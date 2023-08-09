9ae8b3a22d4cddb0f98ad09f5d809579fd91e0c4
Igor Dostovalov <igor.dostovalov@devim.team>
2023-08-09 22:44:46 +0500

    fix dish

--- src/components/Dish/component.jsx
+++ src/components/Dish/component.jsx
@@ -1,29 +1,24 @@
 import { useState } from 'react';
 
-const ADD = 'add';
-const REMOVE = 'remove';
-
 export const Dish = ({ dish }) => {
   const [count, setCount] = useState(0);
 
-  const toggleDish = (type) => {
-    if(type === ADD) {
-      setCount(count => count + 1);
-      return;
-    } 
-
-    if(type === REMOVE && count > 0) {
+  const removeDish = () => {
+    if(count > 0) {
       setCount(count => count - 1);
-      return;
     }
+  }
+
+  const addDish = () => {
+    setCount(count => count + 1);
   }
 
   return <div>
     {dish.name}
     <div>
-      <button onClick={() => toggleDish(REMOVE)}>-</button>
+      <button onClick={removeDish}>-</button>
       <span>{count}</span>
-      <button onClick={() => toggleDish(ADD)}>+</button>
+      <button onClick={addDish}>+</button>
     </div>
     price: {Number(dish.price) *  count}
     </div>;
