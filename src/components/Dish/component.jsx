41eeec1d60f91199481d4b21feac36a8e88f4dff
Daniil <DANIELsclub@yandex.ru>
2023-08-09 17:57:02 +0300

    second hw done

--- src/components/Dish/component.jsx
+++ src/components/Dish/component.jsx
@@ -1,3 +1,29 @@
+import { useState } from "react";
+
 export const Dish = ({ dish }) => {
-  return <div>{dish.name}</div>;
+  
+  const [valueOfDishes, setValueOfDishes] = useState(0)
+
+  const increment = () => {
+    setValueOfDishes(valueOfDishes + 1);
+  }
+  const decrement = () => {
+    valueOfDishes > 0 ? setValueOfDishes(valueOfDishes - 1) : 0;
+  }
+  const changeInputValue = (evt) => {
+    evt.target.value >= 0 ? setValueOfDishes(parseInt(evt.target.value)) : 0;
+  }
+
+  const total = dish.price * valueOfDishes;
+
+  return (
+    <div>
+      <h2>{dish.name}</h2>
+      <button onClick={decrement}>-</button>
+      <input type="number" value = {valueOfDishes} onChange={changeInputValue}/> 
+      <button onClick={increment}>+</button>
+      <p>At all: {total}</p>
+    </div>
+  )
+    
 };
